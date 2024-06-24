61aa7e76efc93441cfed1b737c632778551ea48c
antonio-aag <antonioarias10723@gmail.com>
2024-06-24 16:46:07 -0500

    diseño-login

--- sinut2/settings.py
+++ sinut2/settings.py
@@ -84,6 +84,12 @@ WSGI_APPLICATION = 'sinut2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
 '''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
